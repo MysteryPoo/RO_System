f3da30db3c782242998cc9ffd195aa36989a9058
Matthew Thornton <matt.thornton@knights.ucf.edu>
2021-06-11 17:01:29 -0400

    Component containing a list of log entries

--- WebApp/client/src/components/log.vue
+++ WebApp/client/src/components/log.vue
@@ -1,120 +1,78 @@
 <template>
-  <div class="small">
-    <device-select @deviceSelected="fetchApi"/>
-    <h1 v-if="show">
-      Status: {{ status ? "Online" : `Offline (Last Online: ${lastOnline})` }}
-    </h1>
-    <h1 v-if="show">Current State: {{ status ? currentState : "Offline" }}</h1>
-    <water-level-time-chart
-      v-bind:tickData="lastTick"
-      v-bind:deviceConfig="deviceConfig"
-      v-if="show"
-    ></water-level-time-chart>
-    <time-range-selector v-if="show" @dateUpdated="updateFromDate"/>
-    <resolution-selector v-if="show" @resolutionChanged="updateResolution"/>
-  </div>
+  <b-container fluid>
+    <b-table
+      hover
+      small
+      :items="logs"
+      :fields="fields"
+    >
+      <template #cell(actions)="row">
+        <b-button size="sm" @click="clearLog(deviceId, row.item.dbKey);" class="mr-1">
+          X
+        </b-button>
+      </template>
+    </b-table>
+  </b-container>
 </template>
 
 <script>
 import Api from '@/api/Api';
-import DeviceSelect from './DeviceSelect.vue';
-import WaterLevelTimeChart from './WaterLevelTimeGraph.vue';
-import TimeRangeSelector from './TimeRangeSelector.vue';
-import ResolutionSelector from './ResolutionSelector.vue';
 
 export default {
   name: 'Log',
-  components: {
-    DeviceSelect,
-    WaterLevelTimeChart,
-    TimeRangeSelector,
-    ResolutionSelector,
+  props: {
+    logData: Array,
+    deviceId: String,
   },
   data() {
     return {
-      show: false,
-      lastTick: [],
-      lastOnline: null,
-      status: false,
-      currentState: '',
-      stateList: null,
-      graphSettings: {
-        fromDate: TimeRangeSelector.getDefaults().fromDate,
-        resolution: ResolutionSelector.getDefaults().resolution,
-      },
-      deviceId: null,
-      deviceConfig: null,
-      refreshInterval: null,
+      fields: [
+        {
+          key: 'datetime',
+          sortable: true,
+        },
+        {
+          key: 'criticality',
+          sortable: true,
+        },
+        {
+          key: 'message',
+          sortable: false,
+        },
+        {
+          key: 'actions',
+        },
+      ],
     };
   },
   methods: {
-    async fetchApi(deviceId) {
-      let shouldShow = true;
-      if (deviceId && deviceId !== 'null') {
-        this.deviceId = deviceId;
-        this.deviceConfig = await Api.fetchConfiguration(deviceId);
-        this.status = (await Api.fetchStatus(deviceId)).online;
-        await this.updateGraphSettings(undefined, undefined);
-        if (this.lastTick.length > 0) {
-          this.lastOnline = (new Date(this.lastTick[0].datetime)).toLocaleString();
-        } else {
-          shouldShow = false;
-          this.lastOnline = null;
+    async clearLog(deviceId, logId) {
+      await Api.clearLog(deviceId, logId);
+      this.$emit('refreshLog');
+    },
+  },
+  computed: {
+    logs() {
+      const logs = [];
+      this.logData.forEach((log) => {
+        let colorization = '';
+        if (log.component === 'INFO') {
+          colorization = 'info';
+        } else if (log.component === 'WARN') {
+          colorization = 'warning';
+        } else if (log.component === 'ERROR') {
+          colorization = 'danger';
         }
-        this.stateList = await Api.fetchState(deviceId);
-        if (this.stateList.length > 0) {
-          this.currentState = this.stateList[0].data.state;
-        } else {
-          this.currentState = 'Unknown';
-        }
-      } else {
-        shouldShow = false;
-      }
-      if (!this.refreshInterval) {
-        this.refreshInterval = setInterval(() => {
-          this.fetchApi(this.deviceId);
-        }, 5000);
-      }
-      this.show = shouldShow;
-    },
-    async updateGraphSettings(fromDate, resolution) {
-      this.graphSettings.fromDate = (fromDate || this.graphSettings.fromDate);
-      this.graphSettings.resolution = (resolution || this.graphSettings.resolution);
-      this.lastTick = await Api.fetchTick(
-        this.deviceId,
-        undefined,
-        this.graphSettings.fromDate,
-        this.graphSettings.resolution,
-      );
-    },
-    updateFromDate(fromDate) {
-      this.updateGraphSettings(fromDate, undefined);
-    },
-    updateResolution(resolution) {
-      this.updateGraphSettings(undefined, resolution);
+        logs.push({
+          dbKey: log._id,
+          datetime: new Date(log.datetime),
+          criticality: log.component,
+          message: log.data.message,
+          _rowVariant: colorization,
+        });
+      });
+      return logs;
     },
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-.small {
-  max-width: 600px;
-  margin:  150px auto;
-}
-</style>
