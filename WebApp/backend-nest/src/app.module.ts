bd5ef0e7e76857c52a91912e596acbc2c3f4db58
Matthew Thornton <matt.thornton@knights.ucf.edu>
2022-01-27 12:24:00 -0500

    Auth strategy implemented
    * Missing connection to database
    * Using dummy data

--- WebApp/backend-nest/src/app.module.ts
+++ WebApp/backend-nest/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [],
+  imports: [AuthModule],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
---
+++ WebApp/backend-nest/src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { LocalAuthGuard } from './local-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
