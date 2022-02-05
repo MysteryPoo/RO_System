d4622d5f28d9863bf37740001f34ecb3864f1c55
Matthew Thornton <matt.thornton@knights.ucf.edu>
2022-02-05 12:29:23 -0500

    ISubCallback Interface
    * Added a callback for OnConnect when MQTT reconnects
    * Useful for setting up subscriptions

--- Reverse-Osmosis-Controller/src/MQTT/ISubCallback.h
+++ Reverse-Osmosis-Controller/src/MQTT/ISubCallback.h
@@ -2,10 +2,13 @@
 #ifndef __ISUBCALLBACK_
 #define __ISUBCALLBACK_
 
+class MQTTClient;
+
 class ISubCallback
 {
 public:
   virtual void Callback(char*, uint8_t*, unsigned int) = 0;
+  virtual void OnConnect(bool connected, MQTTClient* mqtt) {};
 };
 
 
