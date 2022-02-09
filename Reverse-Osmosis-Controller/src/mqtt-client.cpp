cd1d0649952c027bfdb5433a65fae92deb5c0009
Matthew Thornton <matt.thornton@knights.ucf.edu>
2022-02-09 14:10:15 -0500

    Only report connected after subscriptions are made

--- Reverse-Osmosis-Controller/src/mqtt-client.cpp
+++ Reverse-Osmosis-Controller/src/mqtt-client.cpp
@@ -89,7 +89,6 @@ void MQTTClient::Update()
         bool connectionSuccess = this->client.connect("sparkclient_" + String(Time.now()), username, password);
         if (this->client.isConnected())
         {
-          this->client.publish("from/" + System.deviceID() + "/status", "connected");
           for(ISubCallback* listener : this->listeners)
           {
             listener->OnConnect(connectionSuccess, this);
@@ -97,6 +96,7 @@ void MQTTClient::Update()
           this->connected = true;
           this->discovery = false;
           udp.stop();
+          this->client.publish("from/" + System.deviceID() + "/status", "connected");
         }
         else
         {
