4a975445fe4264a1b68c1e0236ee33b1a4717a89
Matthew Thornton <matt.thornton@knights.ucf.edu>
2022-03-01 16:39:09 -0500

    Round off the WiFi signal value

--- WebApp/frontend/src/components/DeviceStatus.vue
+++ WebApp/frontend/src/components/DeviceStatus.vue
@@ -10,7 +10,7 @@
             <RemainingTime v-if="currentState === 'FLUSH' || currentState === 'FILL'" :startTime="stateStartTime" :estimatedElapsedSeconds="currentState === 'FLUSH' ? 300 : props.averageFillTime" />
             <p>Firmware Version: <Chip :label="version" /></p>
             <p>System is <span :style="enabled ? 'color: green' : 'color: red'">{{ enabled ? "Enabled" : "Disabled" }}</span></p>
-            <i v-show="wifiSignal" class="pi pi-wifi" :style=wifiColor>{{ wifiSignal }}%</i>
+            <i v-show="wifiSignal" class="pi pi-wifi" :style=wifiColor>{{ Math.round(wifiSignal) }}%</i>
             <FeatureList :deviceId="props.deviceId" />
           </div>
         </template>
