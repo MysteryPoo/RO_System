622483552d359dbc647c4ebbf6f002ea4a081601
Matthew Thornton <matt.thornton@knights.ucf.edu>
2023-04-19 11:20:24 -0400

    Gutting out deprecated services
    * Migrated to Supabase, so MongoDB is no longer required
    * Utilizing Supabase Realtime, so Backend API no longer required
    * Migrated frontend to Quasar, so old Vue native app is deprecated

--- WebApp/backend/src/devices/devices.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('devices/:deviceId')
-export class DevicesController {}
--- WebApp/backend/src/particle/constants.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-const username = process.env.PARTICLE_USERNAME ?? 'admin';
-const password = process.env.PARTICLE_PASSWORD ?? 'admin';
-
-export const particleConstants = {
-  username,
-  password,
-};
